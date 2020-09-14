cc2eb1d84e1f2876e613c31b679e92d2473c0243
Edwin Guerrero <edwinguerrerotech@gmail.com>
2020-09-14 18:39:58 -0400

    Record Button
    - Constraints added to keep the button in the center of the screen.
    - Button is programmed to change color and text after each press.

--- VoiceChanger/ViewController.swift
+++ VoiceChanger/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var recordBtn: UIButton!
+    var recording: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Make record button round
+        recordBtn.layer.cornerRadius = recordBtn.layer.bounds.height/2
     }
-
+    
+    @IBAction func recordBtnPressed(_ sender: Any) {
+        if !recording {
+            // Start Recording
+            recording = true
+            print("Recording...")
+            // Prep for stopping
+            recordBtn.backgroundColor = UIColor.systemRed
+            recordBtn.setTitle("Stop", for: .normal)
+        } else {
+            // Stop Recording
+            recording = false
+            print("Recording Stopped.")
+            // Prep for recording
+            recordBtn.backgroundColor = UIColor.systemGreen
+            recordBtn.setTitle("Record", for: .normal)
+        }
+    }
 
 }
 
