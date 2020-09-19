35ca84585f59b99bde1a83ff1d2960bcbed8a240
Edwin Guerrero <edwinguerrerotech@gmail.com>
2020-09-18 20:00:09 -0400

    View Controller Fixed and Audio Recording Added

--- VoiceChanger/RecordAudioViewController.swift
+++ VoiceChanger/RecordAudioViewController.swift
@@ -11,6 +11,8 @@ import AVFoundation
 
 class RecordAudioViewController: UIViewController {
     
+    var audioRecorder: AVAudioRecorder!
+    
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var recordBtn: UIButton!
     @IBOutlet weak var stopBtn: UIButton!
@@ -25,6 +27,20 @@ class RecordAudioViewController: UIViewController {
         label.text = "Recording in Progress"
         recordBtn.isEnabled = false
         stopBtn.isEnabled = true
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        print(filePath ?? "filePath is nil")
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSession.Category.playAndRecord, options: .defaultToSpeaker)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
     }
     
     @IBAction func stop(_ sender: Any) {
@@ -32,7 +48,10 @@ class RecordAudioViewController: UIViewController {
         label.text = "Tap to Record"
         stopBtn.isEnabled = false
         recordBtn.isEnabled = true
+        
+        audioRecorder.stop()
+        let session = AVAudioSession.sharedInstance()
+        try! session.setActive(false)
     }
-    
 }
 
